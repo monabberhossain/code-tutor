905ba389934c4496a9d6cb7e8a6560d3eeff9d44
monabberhossain <mn2world@gmail.com>
2022-10-27 00:34:25 +0600

    Home route completed

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import BrandCarousel from "../../Shared/BrandCarousel/BrandCarousel";
-import Card1 from "../../../assets/cards/Card1.jpg";
-import Card2 from "../../../assets/cards/Card2.jpg";
+import Feedback1 from "../../../assets/feedbacks/feedback1.jpg";
+import Feedback2 from "../../../assets/feedbacks/feedback2.jpg";
+import Feedback3 from "../../../assets/feedbacks/feedback3.jpg";
+import Feedback4 from "../../../assets/feedbacks/feedback4.jpg";
 
 const Home = () => {
     return (
         <div>
             <BrandCarousel></BrandCarousel>
-
-            <Row xs={1} md={2} lg={3} className="g-4 mt-5">
+            <h1 className="mt-5 text-center text-primary">Learner Feedback</h1>
+            <Row xs={1} md={2} lg={4} className="g-5 mt-0 px-5">
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src={Card1} />
+                        <Card.Img
+                            className="p-3 rounded-circle"
+                            variant="left"
+                            src={Feedback1}
+                        />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
+                            <Card.Title className="text-center text-primary">
+                                Maria Jessica
+                            </Card.Title>
+                            <Card.Text className="text-center text-justify">
                                 This is a longer card with supporting text below
                                 as a natural lead-in to additional content. This
                                 content is a little bit longer.
@@ -25,10 +33,16 @@ const Home = () => {
                 </Col>
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src={Card2} />
+                        <Card.Img
+                            className="p-3 rounded-circle"
+                            variant="top"
+                            src={Feedback2}
+                        />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
+                            <Card.Title className="text-center text-primary">
+                                Andre Fletcher
+                            </Card.Title>
+                            <Card.Text className="text-center text-justify">
                                 This is a longer card with supporting text below
                                 as a natural lead-in to additional content. This
                                 content is a little bit longer.
@@ -38,10 +52,35 @@ const Home = () => {
                 </Col>
                 <Col>
                     <Card>
-                        <Card.Img variant="top" src="" />
+                        <Card.Img
+                            className="p-3 rounded-circle"
+                            variant="top"
+                            src={Feedback3}
+                        />
                         <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
+                            <Card.Title className="text-center text-primary">
+                                Lipa Mulani
+                            </Card.Title>
+                            <Card.Text className="text-center text-justify">
+                                This is a longer card with supporting text below
+                                as a natural lead-in to additional content. This
+                                content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img
+                            className="p-3 rounded-circle"
+                            variant="top"
+                            src={Feedback4}
+                        />
+                        <Card.Body>
+                            <Card.Title className="text-center text-primary">
+                                John Gray
+                            </Card.Title>
+                            <Card.Text className="text-center text-justify">
                                 This is a longer card with supporting text below
                                 as a natural lead-in to additional content. This
                                 content is a little bit longer.
